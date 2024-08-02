a18a5736cc570e7f908271b621639cd81553de65
nilesh <nileshkishore2001@gmail.com>
2024-08-03 03:18:10 +0530

    port.sh

---
+++ port.sh
@@ -0,0 +1,6 @@
+kubectl port-forward -n stock-monitoring svc/backend 8001:8000 &
+kubectl port-forward -n stock-monitoring svc/db 8002:5432 &
+kubectl port-forward -n stock-monitoring svc/frontend 8003:3000 &
+kubectl port-forward -n stock-monitoring svc/grafana 8004:3000 &
+kubectl port-forward -n stock-monitoring svc/metrics-collector 8005:7878 &
+kubectl port-forward -n stock-monitoring svc/prometheus 8006:9090 &
