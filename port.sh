4ec19602fde5e860a56eeb004887c5c95a9ce1a1
nilesh <nileshkishore2001@gmail.com>
2024-08-03 10:38:00 +0530

    fdsa

--- port.sh
+++ port.sh
@@ -1,3 +1,4 @@
+#!bin/bash
 kubectl port-forward -n stock-monitoring svc/backend 8001:8000 &
 kubectl port-forward -n stock-monitoring svc/db 8002:5432 &
 kubectl port-forward -n stock-monitoring svc/frontend 8003:3000 &
